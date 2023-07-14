1054499db8cf77cd7a6e2667fda1b1d0a3986916
Tara J. Massan <70990388+tarajm@users.noreply.github.com>
2023-07-14 14:38:40 -0500

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,21 @@
+#datetime module
+from datetime import datetime
+
+current_time = datetime.now()
+
+print(current_time)
+
+#Random Module
+
+# Import random below:
+import random
+
+# Create random_list below:
+random_list=[random.randint(1,100) for i in range(101)]
+
+# Create randomer_number below:
+randomer_number = random.choice(random_list)
+
+# Print randomer_number below:
+
+print(randomer_number)
