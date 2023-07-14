27eaf67e494a8df78ef31844f0db734cd3544599
Tara J. Massan <70990388+tarajm@users.noreply.github.com>
2023-07-14 14:52:53 -0500

    Update modules.py
    Modules Python Namespaces

--- modules.py
+++ modules.py
@@ -19,3 +19,18 @@ randomer_number = random.choice(random_list)
 # Print randomer_number below:
 
 print(randomer_number)
+
+#Namespaces
+
+# import codecademylib3_seaborn
+# from matplotlib import pyplot as plt
+# import random
+# # Add your code below:
+
+# numbers_a = range(1,13)
+# numbers_b = random.sample(range(1000),12)
+# print(numbers_b)
+
+# plt.plot(numbers_a, numbers_b)
+
+# plt.show()
